a3554090155434b32c132e10fed0df2ffadbb0d2
vinothcs24 <vinothcs24@gmail.com>
2025-04-29 06:04:15 +0000

    ss

--- lib/data/repositories/message_repository_impl.dart
+++ lib/data/repositories/message_repository_impl.dart
@@ -12,15 +12,17 @@ class MessageRepositoryImpl implements MessageRepository {
   DateTime date = sms.date ?? DateTime.now();
   List<Expense> expenses = [];
 
-  print('SMS Body: $body');
+  print('parseMessageToExpense - SMS Body: $body');
 
   // More flexible regex for debit messages, with amount at the start or the end.
   RegExp debitExp = RegExp(
     r"(?:debited|spent|paid|withdrawn|purchase|transfer|txn|payment).*(?:rs\.?|inr\.?|amount|)([\\d,]+\\.?\\d*)|([\\d,]+\\.?\\d*).*(?:debited|spent|paid|withdrawn|purchase|transfer|txn|payment)",
     caseSensitive: false,
   );
+    
+  
   final debitMatches = debitExp.allMatches(body);
-  print('Debit Matches: $debitMatches');
+  print('parseMessageToExpense - Debit Matches: $debitMatches');
 
   for (final debitMatch in debitMatches) {
     String? amountStr = debitMatch.group(1)?.replaceAll(",", "");
@@ -28,52 +30,67 @@ class MessageRepositoryImpl implements MessageRepository {
         amountStr = debitMatch.group(2)?.replaceAll(",", "");
     }
 
+    print('parseMessageToExpense - amountStr: $amountStr');
+    print("parseMessageToExpense - amountStr is empty ${amountStr?.isEmpty}");
+
+
       double? amount = double.tryParse(amountStr ?? '');
 
-    if (amount != null) {
+      print("parseMessageToExpense - amount: $amount");
+
+      if (amount != null) {
+      print('parseMessageToExpense - Extracted Expense: {amount: $amount, type: debit}');
+
       expenses.add(Expense(
         amount: amount,
         type: 'debit',
         date: date,
         description: body,
       ));
-      print('Extracted Expense: {amount: $amount, type: debit}');
+    } else {
+        print('parseMessageToExpense - No debit expense created');
     }
   }
 
   // More flexible regex for credit messages, with amount at the start or the end.
   RegExp creditExp = RegExp(
     r"(?:credited|received|deposit|credit|refund).*(?:rs\.?|inr\.?|amount|)([\\d,]+\\.?\\d*)|([\\d,]+\\.?\\d*).*(?:credited|received|deposit|credit|refund)",
-    caseSensitive: false,
-  );
-  final creditMatches = creditExp.allMatches(body);
-  print('Credit Matches: $creditMatches');
-  for (final creditMatch in creditMatches) {
-    String amountStr = creditMatch.group(1)?.replaceAll(",", "") ?? "";
-    if (amountStr == null || amountStr.isEmpty) {
-        amountStr = creditMatch.group(2)!.replaceAll(",", "");
-    }
-      double? amount = double.tryParse(amountStr ?? '');
+      caseSensitive: false,
+    );
+    final creditMatches = creditExp.allMatches(body);
+    print('parseMessageToExpense - Credit Matches: $creditMatches');
+    
+    for (final creditMatch in creditMatches) {
+        String? amountStr = creditMatch.group(1)?.replaceAll(",", "");
+        if (amountStr == null || amountStr.isEmpty) {
+            amountStr = creditMatch.group(2)?.replaceAll(",", "");
+        }
+        print('parseMessageToExpense - amountStr: $amountStr');
+        print("parseMessageToExpense - amountStr is empty ${amountStr?.isEmpty}");
+        double? amount = double.tryParse(amountStr ?? '');
+        print("parseMessageToExpense - amount: $amount");
+        if (amount != null) {
+        print('parseMessageToExpense - Extracted Expense: {amount: $amount, type: credit}');
 
-    if (amount != null) {
-
-      expenses.add(Expense(
-        amount: amount,
-        type: 'credit',
-        date: date,
-        description: body,
-      ));
-      print('Extracted Expense: {amount: $amount, type: credit}');
+        expenses.add(Expense(
+          amount: amount,
+          type: 'credit',
+          date: date,
+          description: body,
+        ));
+      } else {
+          print('parseMessageToExpense - No credit expense created');
       }
     }
     if(expenses.isEmpty){
-        print('No expense found');
+        print('parseMessageToExpense - No expense found');
     }
     return expenses;
   }
 
   @override
   Future<List<Expense>> getAllExpenses() async {
+    print("getAllExpenses - Starting getAllExpenses");
     var permission = await Permission.sms.request();
     List<Expense> expenses = [];
     if (permission.isGranted || permission.isLimited) {
@@ -84,6 +101,7 @@ class MessageRepositoryImpl implements MessageRepository {
         expenses.addAll(messageExpenses);
         
       }
+      print("getAllExpenses - Number of messages read: ${messages.length}");
       return expenses;
     } else {
       throw Exception("SMS permission not granted");
